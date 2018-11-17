137ccf9d48a57760118093c9b9ad342f8d43539d
Kristoffer <fobber1995@gmail.com>
2018-11-17 17:03:30 +0100

    Mask code in view

--- iSynApp/iSynApp.App/Models/ISynReport.cs
+++ iSynApp/iSynApp.App/Models/ISynReport.cs
@@ -5,6 +5,7 @@ namespace iSynApp.App.Models
     public class ISynReport
     {
         public string Code { get; set; }
+        public string CodeMasked => Code.Substring(0, 5) + "**********" + Code.Substring(15);
         public string Tenancy { get; set; }
         public bool IsClosed { get; set; }
         public string BlueprintUrl { get; set; }
