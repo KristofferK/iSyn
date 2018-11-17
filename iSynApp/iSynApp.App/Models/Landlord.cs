94250053496cd622aa09cbaad83ccaab30c9567d
Kristoffer <fobber1995@gmail.com>
2018-11-17 16:09:03 +0100

    Show info about tenant and landlord

--- iSynApp/iSynApp.App/Models/Landlord.cs
+++ iSynApp/iSynApp.App/Models/Landlord.cs
@@ -7,5 +7,10 @@ namespace iSynApp.App.Models
 {
     public class Landlord
     {
+        public string Name { get; internal set; }
+        public string Department { get; internal set; }
+        public string ApartmentId { get; internal set; }
+        public string Address { get; internal set; }
+        public string ZipCity { get; internal set; }
     }
 }
--- iSynApp/iSynApp.App/Models/Tenant.cs
+++ iSynApp/iSynApp.App/Models/Tenant.cs
@@ -7,5 +7,9 @@ namespace iSynApp.App.Models
 {
     public class Tenant
     {
+        public string Name { get; internal set; }
+        public string Email { get; internal set; }
+        public string PhoneNo { get; internal set; }
+        public string OccupancyDate { get; internal set; }
     }
 }
