b9a8025d9cff365a0dbcb7447346c7a7d6ca2c6a
Kristoffer <fobber1995@gmail.com>
2018-11-18 21:30:22 +0100

    Add Deficiency model

---
+++ iSynApp/iSynApp.App/Models/Deficiency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iSynApp.App.Models
+{
+    public class Deficiency
+    {
+        public string Description { get; set; }
+    }
+}
