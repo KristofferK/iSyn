945d3ce4eeda17880a96a10b820655f6da0f3838
Kristoffer <fobber1995@gmail.com>
2018-11-18 21:30:57 +0100

    Add Room model

---
+++ iSynApp/iSynApp.App/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iSynApp.App.Models
+{
+    public class Room
+    {
+        public string Title { get; set; }
+        public IEnumerable<Deficiency> Deficiencies { get; set; }
+    }
+}
